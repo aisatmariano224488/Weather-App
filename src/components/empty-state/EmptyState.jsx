73732650c96982f2033527601c99f08fc7276119
mariano-riyan <marianoriyan@gmail.com>
2026-07-24 22:05:09 +0800

    feat: Integrate GSAP animations and enhance UI components with animated effects

--- src/components/empty-state/EmptyState.jsx
+++ src/components/empty-state/EmptyState.jsx
@@ -1,13 +1,40 @@
 import { FeaturesCard } from './FeaturesCard';
 import SearchBar from '@/components/search/SearchBar';
+import SplitText from "@/components/SplitText";
 
 const EmptyState = () => {
 
     return (
-        <div className='space-y-16'>
-            <div className='space-y-8 text-center mt-16'>
-                <h1 className='text-5xl lg:text-6xl xl:text-7xl font-bold'>Weather</h1>
-                <p className='text-sm xl:text-base tracking-wide'>Beautifully minimal weather data, engineered for the modern web.</p>
+        <div className='space-y-16 hide-scrollbar overflow-y-scroll'>
+            <div className='gap-8 text-center mt-16 flex flex-col'>
+                <SplitText
+                    text="XWeather"
+                    className="text-5xl lg:text-6xl xl:text-7xl font-bold"
+                    delay={30}
+                    duration={0.9}
+                    ease="power3.out"
+                    splitType="words"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                    showCallback={false}
+                />
+                <SplitText
+                    text="Beautifully minimal weather data, engineered for the modern web."
+                    className="text-sm xl:text-base tracking-wide"
+                    delay={30}
+                    duration={0.9}
+                    ease="power3.out"
+                    splitType="words"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                    showCallback={false}
+                />
 
                 <SearchBar />
             </div>
--- src/components/empty-state/FeaturesCard.jsx
+++ src/components/empty-state/FeaturesCard.jsx
@@ -1,7 +1,8 @@
 import { Card, CardDescription, CardHeader, CardTitle, } from "@/components/ui/Card";
 
-import { ClockCheck, MapPinSearch, Thermometer } from 'lucide-react';
+import AnimatedContent from '@/components/AnimatedContent';
 import { getFeaturesData } from '@/services/featuresService';
+import { ClockCheck, MapPinSearch, Thermometer } from 'lucide-react';
 
 export const FeaturesCard = () => {
 
@@ -20,15 +21,28 @@ export const FeaturesCard = () => {
                 const Icon = icons[feature.icon];
 
                 return (
-                    <Card key={feature.id} className="h-full p-10 md:p-6 lg:p-10 space-y-4">
-                        {Icon && 
-                            <Icon size={50} className='self-center p-3 bg-accent rounded-full' />
-                        }
-                        <CardHeader className='text-center tracking-wide space-y-2'>
-                            <CardTitle className="font-semibold text-xs lg:text-sm uppercase">{feature.name}</CardTitle>
-                            <CardDescription>{feature.desc}</CardDescription>
-                        </CardHeader>
-                    </Card>
+                    <AnimatedContent
+                        distance={100}
+                        direction="vertical"
+                        reverse={false}
+                        duration={0.8}
+                        ease="power3.out"
+                        initialOpacity={0}
+                        animateOpacity
+                        scale={1}
+                        threshold={0.1}
+                        delay={feature.id * 0.1}
+                    >
+                        <Card key={feature.id} className="h-full p-10 md:p-6 lg:p-10 space-y-4">
+                            {Icon && 
+                                <Icon size={50} className='self-center p-3 bg-accent rounded-full' />
+                            }
+                            <CardHeader className='text-center tracking-wide space-y-2'>
+                                <CardTitle className="font-semibold text-xs lg:text-sm uppercase">{feature.name}</CardTitle>
+                                <CardDescription>{feature.desc}</CardDescription>
+                            </CardHeader>
+                        </Card>
+                    </AnimatedContent>
                 )
             })}
         </div>
